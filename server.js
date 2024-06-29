83912c68d85d35040e2d38db305e7c3f2282ac12
cheang120 <babycode.2023@gmail.com>
2024-06-29 17:35:32 +0800

    ..

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(
     cors({
-        origin:["http://localhost:3000", "https//https://e-shop-two-iota.vercel.app"],
+        origin:["http://localhost:3000", "https://e-shop-two-iota.vercel.app"],
         credentials:true
     })
 )
